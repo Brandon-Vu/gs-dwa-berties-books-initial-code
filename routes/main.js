b5068c2d68c47dcf23fd9b51ea0ee04b9cb4d520
Brandon-Vu <brandonvu20052005@gmail.com>
2025-11-27 15:48:38 +0000

    routing fix

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ const router = express.Router()
 // Redirect middleware for protected routes
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    return res.redirect('/users/login')
+    return res.redirect('./login')
   }
   next()
 }
